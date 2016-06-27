ccc6bd39707061fe50fa7e83a13ee38fbfdbe9ec
Jon Schneider <jkschneider@gmail.com>
2016-06-27 10:53:07 -0700

    Try to recreate issue 38

---
+++ issues/issue38/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Main {
+}
