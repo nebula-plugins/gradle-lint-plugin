abe3094d9d7b8463f16f25923c374bb4522fedac
Jon Schneider <jkschneider@gmail.com>
2016-07-28 17:58:31 -0700

    Attempt to recreate issue #41

--- src/test/groovy/com/netflix/nebula/lint/plugin/GradleLintReportTaskSpec.groovy
+++ src/test/groovy/com/netflix/nebula/lint/plugin/GradleLintReportTaskSpec.groovy
@@ -25,11 +25,48 @@ class GradleLintReportTaskSpec extends TestKitSpecification {
         """
 
         then:
-        def results = runTasksSuccessfully('generateGradleLintReport')
+        runTasksSuccessfully('generateGradleLintReport')
 
         when:
         def report = new File(projectDir, 'build/reports/gradleLint').listFiles().find { it.name.endsWith('.txt') }
 
+        then:
+        report.text.contains('Violation: Rule=dependency-parentheses')
+        report.text.contains('TotalFiles=1')
+    }
+    
+    def 'generate a report for a multi-module project'() {
+        when:
+        buildFile.text = """
+            plugins {
+                id 'nebula.lint'
+            }
+        """
+        
+        def sub = addSubproject('sub')
+        new File(sub, 'build.gradle').text = """
+            plugins {
+                id 'java'
+            }
+
+            gradleLint {
+                rules = ['dependency-parentheses']
+                reportFormat = 'text'
+            }
+
+            repositories { mavenCentral() }
+
+            dependencies {
+                compile('com.google.guava:guava:18.0')
+            }
+        """
+
+        then:
+        runTasksSuccessfully('generateGradleLintReport')
+
+        when:
+        def report = new File(projectDir, 'build/reports/gradleLint').listFiles().find { it.name.endsWith('.txt') }
+        
         then:
         report.text.contains('Violation: Rule=dependency-parentheses')
         report.text.contains('TotalFiles=1')
