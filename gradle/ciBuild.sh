4e8111101cf072fc0e5309d7517573c8822aa494
Rob Spieldenner <rob.spieldenner@gmail.com>
2017-01-06 11:11:35 -0800

    Add clean to make sure build issues aren't a dirty repo

--- gradle/ciBuild.sh
+++ gradle/ciBuild.sh
@@ -5,23 +5,23 @@ SWITCHES="-s --console=plain"
 
 if [ $CIRCLE_PR_NUMBER ]; then
   echo -e "Build Pull Request #$CIRCLE_PR_NUMBER => Branch [$CIRCLE_BRANCH]"
-  ./gradlew build $SWITCHES
+  ./gradlew clean build $SWITCHES
 elif [ -z $CIRCLE_TAG ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$CIRCLE_BRANCH']'
-  ./gradlew -Prelease.disableGitChecks=true snapshot $SWITCHES
+  ./gradlew -Prelease.disableGitChecks=true clean snapshot $SWITCHES
 elif [ $CIRCLE_TAG ]; then
   echo -e 'Build Branch for Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']'
   case "$CIRCLE_TAG" in
   *-rc\.*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true clean candidate $SWITCHES
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true clean final $SWITCHES
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']  Pull Request ['$CIRCLE_PR_NUMBER']'
-  ./gradlew build $SWITCHES
+  ./gradlew clean build $SWITCHES
 fi
 
 EXIT=$?
