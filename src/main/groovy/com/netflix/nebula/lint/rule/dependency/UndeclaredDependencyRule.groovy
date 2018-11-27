3803b79bd1dab410e4db8fe67b8f1a4d1fb61fb5
Martin Chalupa <mchalupa@netflix.com>
2018-11-27 13:02:16 -0800

    Refactoring of undeclared dependencies rule. Removing unneccessary logic for dependencies blocks in allprojects and subprojects

--- src/main/groovy/com/netflix/nebula/lint/rule/dependency/UndeclaredDependencyRule.groovy
+++ src/main/groovy/com/netflix/nebula/lint/rule/dependency/UndeclaredDependencyRule.groovy
@@ -1,32 +1,15 @@
 package com.netflix.nebula.lint.rule.dependency
 
-import com.google.common.collect.ImmutableMap
 import com.netflix.nebula.lint.rule.GradleLintRule
 import com.netflix.nebula.lint.rule.GradleModelAware
 import org.codehaus.groovy.ast.ASTNode
 import org.codehaus.groovy.ast.ClassNode
-import org.codehaus.groovy.ast.expr.Expression
 import org.codehaus.groovy.ast.expr.MethodCallExpression
 import org.gradle.api.artifacts.ModuleVersionIdentifier
 import org.gradle.api.plugins.JavaPluginConvention
 
 class UndeclaredDependencyRule extends GradleLintRule implements GradleModelAware {
-    private static final String SUBPROJECTS_BLOCK = 'subprojectsBlock'
-    private static final String ALLPROJECTS_BLOCK = 'allprojectsBlock'
-    private static final String ROOT_BLOCK = 'rootBlock'
-    private static final String SUBPROJECTS_DEPENDENCIES_BLOCK = 'subprojectsDependenciesBlock'
-    private static final String ALLPROJECTS_DEPENDENCIES_BLOCK = 'allprojectsDependenciesBlock'
-    private static final String ROOT_DEPENDENCIES_BLOCK = 'rootDependenciesBlock'
-    private static final List<String> BLOCKS_TO_SEARCH_IN = [
-            ALLPROJECTS_BLOCK,
-            SUBPROJECTS_BLOCK,
-            ROOT_BLOCK
-    ]
-    private static final Map<String, String> DEPENDENCY_BLOCK_ASSOCIATIONS = ImmutableMap.builder()
-            .put(ALLPROJECTS_BLOCK, ALLPROJECTS_DEPENDENCIES_BLOCK)
-            .put(SUBPROJECTS_BLOCK, SUBPROJECTS_DEPENDENCIES_BLOCK)
-            .put(ROOT_BLOCK, ROOT_DEPENDENCIES_BLOCK)
-            .build()
+    private static final String DEPENDENCIES_BLOCK = 'rootDependenciesBlock'
 
     String description = 'Ensure that directly used transitives are declared as first order dependencies, for Gradle versions 4.5 - 5'
     DependencyService dependencyService
@@ -37,29 +20,11 @@ class UndeclaredDependencyRule extends GradleLintRule implements GradleModelAwar
     }
 
     @Override
-    void visitAllprojects(MethodCallExpression call) {
-        bookmark(ALLPROJECTS_BLOCK, call)
-    }
-
-    @Override
-    void visitSubprojects(MethodCallExpression call) {
-        bookmark(SUBPROJECTS_BLOCK, call)
-    }
-
-    @Override
-    void visitClass(ClassNode classNode) {
-        bookmark(ROOT_BLOCK, classNode)
-    }
-
-    @Override
     void visitDependencies(MethodCallExpression call) {
         def parentNode = parentNode()
-        if (parentIs(parentNode, 'allprojects')) {
-            bookmark(ALLPROJECTS_DEPENDENCIES_BLOCK, call)
-        } else if (parentIs(parentNode, 'subprojects')) {
-            bookmark(SUBPROJECTS_DEPENDENCIES_BLOCK, call)
-        } else if (parentNode == null) {
-            bookmark(ROOT_DEPENDENCIES_BLOCK, call)
+        if (parentNode == null) {
+            //bookmark only current project dependencies (not allprojects and subprojects)
+            bookmark(DEPENDENCIES_BLOCK, call)
         }
     }
 
@@ -68,39 +33,36 @@ class UndeclaredDependencyRule extends GradleLintRule implements GradleModelAwar
         Set<ModuleVersionIdentifier> insertedDependencies = [] as Set
         Map<String, HashMap<String, ASTNode>> violations = new HashMap()
 
-        BLOCKS_TO_SEARCH_IN.each { currentBlock ->
-            if (bookmark(currentBlock) != null) {
-                def convention = project.convention.findPlugin(JavaPluginConvention)
-                if (convention != null) {
-                    // sort the sourceSets from least dependent to most dependent, e.g. [main, test, integTest]
-                    def sortedSourceSets = convention.sourceSets.sort(false, dependencyService.sourceSetComparator())
+        def convention = project.convention.findPlugin(JavaPluginConvention)
+        if (convention != null) {
+            // sort the sourceSets from least dependent to most dependent, e.g. [main, test, integTest]
+            def sortedSourceSets = convention.sourceSets.sort(false, dependencyService.sourceSetComparator())
 
-                    sortedSourceSets.each { sourceSet ->
-                        def confName = sourceSet.compileConfigurationName
-                        violations.put(confName, new HashMap())
+            sortedSourceSets.each { sourceSet ->
+                def confName = sourceSet.compileConfigurationName
+                violations.put(confName, new HashMap())
 
-                        dependencyService.undeclaredDependencies(confName).each { undeclared ->
+                def undeclaredDependencies = dependencyService.undeclaredDependencies(confName)
+                if (! undeclaredDependencies.isEmpty()) {
+                    def dependencyBlock = bookmark(DEPENDENCIES_BLOCK)
+                    if (dependencyBlock != null) {
+                        undeclaredDependencies.each { undeclared ->
                             // only add the dependency in the lowest configuration that requires it
                             if (insertedDependencies.add(undeclared)) {
-                                def dependencyBlock = bookmark(DEPENDENCY_BLOCK_ASSOCIATIONS.get(currentBlock))
-                                if (dependencyBlock != null) {
-
-                                    // collect violations for handling later
-                                    HashMap<String, ASTNode> violationsForConfig = violations.get(confName)
-                                    violationsForConfig.put(undeclared.toString(), dependencyBlock)
-
-                                    violations.put(confName, violationsForConfig)
-                                } else if (parentNode() == null) {
-                                    // there is no dependencies block, and this is not in an allprojects or subprojects block
-                                    addBuildLintViolation("one or more classes in $undeclared are required by your code directly, " +
-                                            "and you require a dependencies block in your subproject $project")
-                                            .insertAfter(project.buildFile, 0, """\
+                                // collect violations for handling later
+                                HashMap<String, ASTNode> violationsForConfig = violations.get(confName)
+                                violationsForConfig.put(undeclared.toString(), dependencyBlock)
+                                violations.put(confName, violationsForConfig)
+                            }
+                        }
+                    } else {
+                        // there is no dependencies block so we need a new one
+                        addBuildLintViolation("one or more classes are required by your code directly, " +
+                                "and you require a dependencies block in your subproject $project")
+                                .insertAfter(project.buildFile, 0, """\
                                         dependencies {
                                         }
                                         """.stripIndent())
-                                }
-                            }
-                        }
                     }
                 }
             }
@@ -126,9 +88,5 @@ class UndeclaredDependencyRule extends GradleLintRule implements GradleModelAwar
                         .insertIntoClosureAtTheEnd(dependencyBlock, "$configurationName '$undeclaredDependency'")
             }
         }
-    }
-
-    private static Boolean parentIs(Expression parentNode, String parentAsString) {
-        parentNode instanceof MethodCallExpression && ((MethodCallExpression) parentNode).methodAsString == parentAsString
     }
 }
