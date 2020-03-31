f3b9804844b7debfedd9ba89dd7205f812d1e0a8
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-03-30 17:31:34 -0700

    Fix usage of skipForTasks for GradleLintExtension (#295)
    * Fix usage of skipForTasks for GradleLintExtension
    
    * skipForTasks works with shorthand task names

--- src/main/groovy/com/netflix/nebula/lint/plugin/GradleLintPluginTaskConfigurer.groovy
+++ src/main/groovy/com/netflix/nebula/lint/plugin/GradleLintPluginTaskConfigurer.groovy
@@ -94,7 +94,7 @@ class GradleLintPluginTaskConfigurer extends AbstractLintPluginTaskConfigurer {
         if (!hasValidTaskConfiguration(project, lintExt) || hasExplicitLintTask) {
             return
         }
-        finalizeAllTasksWithAutoLint(project, lintTasksToVerify, autoLintTask)
+        finalizeAllTasksWithAutoLint(project, lintTasksToVerify, autoLintTask, lintExt)
 
     }
 
@@ -140,14 +140,16 @@ class GradleLintPluginTaskConfigurer extends AbstractLintPluginTaskConfigurer {
      * @param lintTasks
      * @param autoLintTask
      */
-    private void finalizeAllTasksWithAutoLint(Project project, List<TaskProvider> lintTasks, Task autoLintTask) {
+    private void finalizeAllTasksWithAutoLint(Project project, List<TaskProvider> lintTasks, Task autoLintTask, GradleLintExtension lintExt) {
         project.tasks.configureEach { task ->
-            if (!lintTasks.contains(task) && !task.name.contains(AUTO_LINT_GRADLE) && !task.name.contains(CLEAN_TASK_NAME) && !task.name.contains(DEPENDENCY_INSIGHT_TASK_NAME)) {
+            boolean skipForSpecificTask = lintExt.skipForTasks.any { taskToSkip -> task.name.endsWith(taskToSkip) }
+
+            if (!lintTasks.contains(task) && !task.name.contains(AUTO_LINT_GRADLE) && !task.name.contains(CLEAN_TASK_NAME) && !skipForSpecificTask) {
                 task.finalizedBy autoLintTask
             }
         }
         project.childProjects.values().each { subProject ->
-            finalizeAllTasksWithAutoLint(subProject, lintTasks, autoLintTask)
+            finalizeAllTasksWithAutoLint(subProject, lintTasks, autoLintTask, lintExt)
         }
     }
 
