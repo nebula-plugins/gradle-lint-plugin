095c7d3611bb8b7514f70c75a073b096ce9ff032
Danny Thomas <dannyt@netflix.com>
2020-10-28 15:35:37 +1100

    Switch to Enterprise plugin

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will build the project.
 
-SWITCHES="--stacktrace --scan"
+SWITCHES="--stacktrace"
 
 GRADLE_VERSION=$(./gradlew -version | grep Gradle | cut -d ' ' -f 2)
 
