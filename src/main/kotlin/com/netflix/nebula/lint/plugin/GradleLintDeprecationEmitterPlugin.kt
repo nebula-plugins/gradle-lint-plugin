000a4a98d44d708c1e2d30c1c3b7ab6be28e4de2
Danny Thomas <dannyt@netflix.com>
2020-10-30 11:28:57 +1100

    Move out of inner classes to avoid having to serialize the parent class

--- src/main/kotlin/com/netflix/nebula/lint/plugin/GradleLintDeprecationEmitterPlugin.kt
+++ src/main/kotlin/com/netflix/nebula/lint/plugin/GradleLintDeprecationEmitterPlugin.kt
@@ -29,56 +29,56 @@ class GradleLintDeprecationEmitterPlugin @Inject constructor(private val emitter
                     listeners.add(DeprecationGradleLintInfoBrokerAction(emitter))
                 })
     }
+}
 
-    private class DeprecationGradleLintInfoBrokerAction(private val emitter: BuildOperationProgressEventEmitter) : GradleLintViolationAction(), Serializable {
-        override fun lintFinished(violations: Collection<GradleViolation>) {
-            violations.forEach(Consumer { violation: GradleViolation ->
-                val details: DeprecatedUsageProgressDetails = ViolationDeprecatedUsageProgressDetails(violation)
-                emitter.emitNowIfCurrent(details)
-            })
+class DeprecationGradleLintInfoBrokerAction(private val emitter: BuildOperationProgressEventEmitter) : GradleLintViolationAction(), Serializable {
+    override fun lintFinished(violations: Collection<GradleViolation>) {
+        violations.forEach(Consumer { violation: GradleViolation ->
+            val details: DeprecatedUsageProgressDetails = ViolationDeprecatedUsageProgressDetails(violation)
+            emitter.emitNowIfCurrent(details)
+        })
+    }
+}
+
+/*
+ * There's an implicit contract between the format of these messages and export of lint violations to Nebula metrics. Tread with care.
+ */
+class ViolationDeprecatedUsageProgressDetails(private val violation: GradleViolation) : DeprecatedUsageProgressDetails {
+    override fun getSummary(): String {
+        val rule = violation.rule
+        // Do not change the position or format of the rule name and severity in brackets
+        return String.format("Lint rule was violated: %s [%s:%s]", violation.message, rule.name, rule.priority)
+    }
+
+    override fun getRemovalDetails(): String {
+        return if (violation.sourceLine != null) "Source line: " + violation.sourceLine else "Refer to the full lint report for more details"
+    }
+
+    override fun getAdvice(): String? {
+        return null
+    }
+
+    override fun getContextualAdvice(): String? {
+        return null
+    }
+
+    override fun getDocumentationUrl(): String? {
+        return violation.documentationUri
+    }
+
+    override fun getType(): String {
+        return if (violation.lineNumber != null) {
+            DeprecatedFeatureUsage.Type.USER_CODE_DIRECT.name
+        } else {
+            DeprecatedFeatureUsage.Type.BUILD_INVOCATION.name
         }
     }
 
-    /*
-     * There's an implicit contract between the format of these messages and export of lint violations to Nebula metrics. Tread with care.
-     */
-    private class ViolationDeprecatedUsageProgressDetails(private val violation: GradleViolation) : DeprecatedUsageProgressDetails {
-        override fun getSummary(): String {
-            val rule = violation.rule
-            // Do not change the position or format of the rule name and severity in brackets
-            return String.format("Lint rule was violated: %s [%s:%s]", violation.message, rule.name, rule.priority)
-        }
-
-        override fun getRemovalDetails(): String {
-            return if (violation.sourceLine != null) "Source line: " + violation.sourceLine else "Refer to the full lint report for more details"
-        }
-
-        override fun getAdvice(): String? {
-            return null
-        }
-
-        override fun getContextualAdvice(): String? {
-            return null
-        }
-
-        override fun getDocumentationUrl(): String? {
-            return violation.documentationUri
-        }
-
-        override fun getType(): String {
-            return if (violation.lineNumber != null) {
-                DeprecatedFeatureUsage.Type.USER_CODE_DIRECT.name
-            } else {
-                DeprecatedFeatureUsage.Type.BUILD_INVOCATION.name
-            }
-        }
-
-        override fun getStackTrace(): List<StackTraceElement> {
-            val fileName = violation.file.absolutePath
-            val lineNumber = violation.lineNumber ?: -1
-            // Do not change the declaringClass or methodName
-            val element = StackTraceElement("build", "lint", fileName, lineNumber)
-            return listOf(element)
-        }
+    override fun getStackTrace(): List<StackTraceElement> {
+        val fileName = violation.file.absolutePath
+        val lineNumber = violation.lineNumber ?: -1
+        // Do not change the declaringClass or methodName
+        val element = StackTraceElement("build", "lint", fileName, lineNumber)
+        return listOf(element)
     }
 }
