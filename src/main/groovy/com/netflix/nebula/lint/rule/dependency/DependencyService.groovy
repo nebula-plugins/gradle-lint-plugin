3dd2014ede6bcd44902ffca34aa7ea02825d96f5
Michael Ruocco <michael.ruocco@hotmail.com>
2020-11-01 20:31:25 +0000

    removes name check and just skips directories instead

--- src/main/groovy/com/netflix/nebula/lint/rule/dependency/DependencyService.groovy
+++ src/main/groovy/com/netflix/nebula/lint/rule/dependency/DependencyService.groovy
@@ -178,7 +178,7 @@ class DependencyService {
         def configurations = resolvableConfigurations()
         for (Configuration conf in configurations) {
             def artifact = conf.resolvedConfiguration.resolvedArtifacts.find { it.moduleVersion.id.module == id }
-            if (artifact && !isTestFixturesDirectory(id, artifact.file)) {
+            if (artifact && !artifact.file.isDirectory()) {
                 return jarContents(artifact.file)
             }
         }
@@ -647,10 +647,6 @@ class DependencyService {
 
         def androidReleaseOutput = project.tasks.findByName('compileReleaseJavaWithJavac')?.destinationDir
         return androidReleaseOutput
-    }
-
-    private static boolean isTestFixturesDirectory(ModuleIdentifier id, File file) {
-        return id.name.endsWith("test-fixtures") && file.isDirectory()
     }
 
     Comparator<? super SourceSet> sourceSetComparator() {
