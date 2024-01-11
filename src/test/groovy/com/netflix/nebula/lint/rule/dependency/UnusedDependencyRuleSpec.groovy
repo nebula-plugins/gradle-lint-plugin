3eeb7a5720f550071bd5808ef84309f953a8bbfa
kramdar <kramdar@plegnicpixels.games>
2024-01-11 11:32:29 -0500

    Make test fail for the right reason

--- src/test/groovy/com/netflix/nebula/lint/rule/dependency/UnusedDependencyRuleSpec.groovy
+++ src/test/groovy/com/netflix/nebula/lint/rule/dependency/UnusedDependencyRuleSpec.groovy
@@ -689,6 +689,7 @@ class UnusedDependencyRuleSpec extends BaseIntegrationTestKitSpec {
             repositories { mavenCentral() }
 
             dependencies {
+                implementation 'com.google.guava:guava:18.0'
                 gradleLint.ignore('unused-dependency') {
                     implementation 'org.apache.httpcomponents:httpclient:4.5.3'
                 }
